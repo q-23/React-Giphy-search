3509052f4b365a1b3a89d07ccfaaf7d21e05b3dd
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2018-06-01 23:36:28 +0200

    Add handle search function

--- components/App.js
+++ components/App.js
@@ -7,16 +7,32 @@ App = React.createClass({
             width: '90%'
         };
 
+
+        handleSearch: function(searchingText) {  // load string input
+            this.setState({
+              loading: true  // signalize that loading has began
+            });
+            this.getGif(searchingText, function(gif) {  // begin GIF downloading
+              this.setState({  // end of loading
+                loading: false,  // cease signalising loading 
+                gif: gif,  // set new gif from loading result
+                searchingText: searchingText  // set new state for searched text
+              });
+            }.bind(this));
+          },
+
         return (
           <div style={styles}>
                 <h1>Wyszukiwarka GIFow!</h1>
                 <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-                <Search/>
-            <Gif
-                loading={this.state.loading}
-                url={this.state.gif.url}
-                sourceUrl={this.state.gif.sourceUrl}
-            />
+                <Search 
+                    onSearch={this.handleSearch}
+                />
+                <Gif
+                    loading={this.state.loading}
+                    url={this.state.gif.url}
+                    sourceUrl={this.state.gif.sourceUrl}
+                />
           </div>
         );
     }
